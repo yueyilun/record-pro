a8b6b3774c7f4c9b695d0afbb867f1c32d1aceab
yueyilun <634331705@qq.com>
2022-09-09 17:15:06 +0800

    first

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+// 该文件是入口文件
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+// 引入根组件
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// 渲染根组件到一个id为root的根节点上，该节点再index.html中
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
